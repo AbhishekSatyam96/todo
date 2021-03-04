c4fcd8c3a0e3106e0edaaa7dc54544fff44b57fb
Abhishek kumar satyam <abhishek.satyam96@gmail.com>
2021-03-04 23:51:42 +0530

    first commit

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css'
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="footer-text">
+                    I am footer
+                </div>
+            </div>
+        )
+    }
+}
